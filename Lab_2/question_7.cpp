75aa2d2217852908fef88e2eff9bf01845014237
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-04-29 16:26:50 +0500

    Question 7 has completed

---
+++ Lab_2/question_7.cpp
@@ -0,0 +1,19 @@
+//Question 7 : Generating fibonacci Series
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Name: Abdul Karim Bukhsh Ansari \nRoll Number :SE-23052\n";
+    int i = 0;  //initializing the starting two values
+    int j = 1;
+    int end;
+    cout<<"Enter the value of number of terms: ";
+    cin>>end;
+    for (int number= 0 ;number<end-1;number++){
+        int num = i+j;  // as we know that the value is the sum of last two values
+        if(number==0) cout<<i<<" "; // because we want to start the series with zero
+        cout<<num<<" ";
+        i=j; 
+        j = num;
+    }
+
+}
