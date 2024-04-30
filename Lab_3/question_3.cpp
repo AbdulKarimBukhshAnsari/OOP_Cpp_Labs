38a3af659d47f41bde31058f6ff1839187bb3f06
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-04-30 14:50:27 +0500

    Question 3 has been completed

---
+++ Lab_3/question_3.cpp
@@ -0,0 +1,25 @@
+// Question 3 : Swapping the number 
+#include<iostream>
+using namespace std;
+
+struct Number{
+    int a;
+    int b;
+};
+
+void swapp(int &a,int &b){
+    int temp = a;
+    a = b;
+    b=temp;
+}
+int main(){
+    cout<<"Name: Abdul Karim Bukhsh Ansari \nRoll Number :SE-23052\n";
+    Number num;
+    cout<<"Enter 1st number: ";
+    cin>>num.a;
+    cout<<"Enter 2nd number: ";
+    cin>>num.b;
+    cout<<"Number before swapping are "<<num.a<<" and "<<num.b<<endl;
+    swapp(num.a,num.b);
+    cout<<"Number after swapping are "<<num.a<<" and "<<num.b;
+}
