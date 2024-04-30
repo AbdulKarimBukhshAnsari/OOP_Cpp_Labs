6e2108a937cae4af4c96c2088e1da06ceaa1a54a
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-04-30 11:36:48 +0500

    Question 10 has been completed

---
+++ Lab_2/question_10.cpp
@@ -0,0 +1,19 @@
+// Question 10 : Day of the week based on the number of days
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Name: Abdul Karim Bukhsh Ansari \nRoll Number :SE-23052\n";
+    string days[7] = {"Mondays","Tuesday","Wednesday","Thursday","Fridays","Saturday","Sunday"};
+    int num_of_day;
+    cout<<"Enter the day number (1-7) : ";
+    cin>>num_of_day;
+    cout<<days[num_of_day-1];
+
+
+
+
+
+
+
+
+}
